b1d999c3dd07a44cb8d2323eaced3720c755e2fa
chavp <my.parinya@gmail.com>
2015-09-19 13:28:29 +0700

    cahnge label msg alt
    Signed-off-by: chavp <my.parinya@gmail.com>

--- src/WordStore.Web/Controllers/WordController.cs
+++ src/WordStore.Web/Controllers/WordController.cs
@@ -46,7 +46,7 @@ namespace WordStore.Web.Controllers
             TempData["alert"] = new AlertMessage
             {
                 Type = "alert-success",
-                Message = "Delete successfully.",
+                Message = string.Format("Delete Word id {0} successfully.", id),
                 Title = "Well done!"
             };
 
@@ -66,7 +66,7 @@ namespace WordStore.Web.Controllers
             TempData["alert"] = new AlertMessage
             {
                 Type = "alert-success",
-                Message = "Save successfully.",
+                Message = string.Format("Save Word id {0} successfully.", saveVM.Id),
                 Title = "Well done!"
             };
 
